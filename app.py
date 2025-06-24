052ebb2be0c8f1540dc2ae5d79d212e76cfa86f6
Paulo Henrique <henriquepauloster@gmail.com>
2025-06-24 20:28:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -44,11 +44,11 @@ def banco(sql):
     try:
         # Conexão com o banco de dados PostgreSQL
         conn = psycopg2.connect(
-            host = "dpg-d1ct6mje5dus73d8abeg-a.oregon-postgres.render.com",
+            host = "dpg-d1dis93e5dus73dk68s0-a.oregon-postgres.render.com",
             port = "5432",
-            dbname = "dbapp_g55c",
-            user = "dbapp_g55c_user",
-            password = "oE7gSUHVKepzRrvIRzVTpBNtwbScHqLT"
+            dbname = "dbtodolist_u280",
+            user = "dbtodolist_u280_user",
+            password = "Z4umZi8oChLhcLHJfQ8A3V35pS61fNlF"
         )
         cursor = conn.cursor() # cursor vai ser a variável para executar os comandos SQL.
         cursor.execute(sql) # executa o comando sql seja insert, select .. etc
